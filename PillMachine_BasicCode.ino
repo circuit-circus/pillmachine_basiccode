ba7c2da0eaf3d4596580fdf5ee9734043cd62d0b
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2017-09-20 12:25:20 +0200

    Code for How Diverse is Your Bacteria

--- PillMachine_BasicCode.ino
+++ PillMachine_BasicCode.ino
@@ -1,31 +1,32 @@
 /* MTG PILLMACHINE MASTER
-moja & circuitcircus
-15.05.2017: Påbegyndt moja
-15.05: Ethernet pastet ind (men ikke tilrettet)
-16.05: Ethernet virker (men submitter blot en dummy til debug)
-10.07: Tilrettelse af formatering og forberedelse på Github
-01.08: Userval="" case skipper submit via ethernet (f.eks. hvis brugeren ikke har sat et stik i Hvor På Kroppen)
+  moja & circuitcircus
+  15.05.2017: Påbegyndt moja
+  15.05: Ethernet pastet ind (men ikke tilrettet)
+  16.05: Ethernet virker (men submitter blot en dummy til debug)
+  10.07: Tilrettelse af formatering og forberedelse på Github
+  01.08: Userval="" case skipper submit via ethernet (f.eks. hvis brugeren ikke har sat et stik i Hvor På Kroppen)
 */
 
 /* CONNECTIONS: 522 RFID --------
-*  RFID SDA  -> 8
-*  RFID SCK  -> 13
-*  RFID MOSI -> 11
-*  RFID MISO -> 12
-*  RFID IRQ  -> ikke forbundet
-*  RFID GND  -> gnd
-*  RFID RST  -> 9
-*  RFID 3.3V -> 3.3V
-*
-*  Hvid LED  -> 2
-*
-*  -----------------------------
+   RFID SDA  -> 8
+   RFID SCK  -> 13
+   RFID MOSI -> 11
+   RFID MISO -> 12
+   RFID IRQ  -> ikke forbundet
+   RFID GND  -> gnd
+   RFID RST  -> 9
+   RFID 3.3V -> 3.3V
+
+   Hvid LED  -> 2
+
+   -----------------------------
 */
 
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Ethernet.h>
 // Define machine individual includes here
+#include <Adafruit_NeoPixel.h>
 
 
 #define maskinNR 1 //FOR AT VI VED HVILKEN STATION DER SUBMITTER
@@ -37,7 +38,7 @@ moja & circuitcircus
 
 static uint8_t mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };
 static uint8_t myip[] = {  10, 0, 0, 100 };
-IPAddress pc_server(10,0,0,31);  // serverens adress
+IPAddress pc_server(10, 0, 0, 31); // serverens adress
 
 boolean cardPresent = false; // DEBUG: Set this and isDebugging to true to test UI
 boolean isDebugging = false; // DEBUG: Set this and cardPresent to true to test UI
@@ -45,24 +46,64 @@ boolean cardPresent_old = false;
 String cardID = ""; // NB skal muligvis laves til char-array for at spare memory
 String cardID_old = "";
 
-String   userval ="";
+String userval = "";
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 EthernetClient client;
 
 // Define machine individual variables here
 
+/*
+ * 
+ * This is the Layout that both LEDs and Buttons follow
+ * 
+ *        -------
+ *   3 o -       - o 2
+ *       |       |  
+ *   4 o -       - o 1
+ *       |       |
+ *   5 o -       - o 0
+ * 
+*/
+
+#define LENGTH 6
+
+const int buttonPins[LENGTH] = {A0, A1, A2, A3, A4, A5};
+int buttonStates[LENGTH];
+int lastButtonStates[LENGTH] = {LOW, LOW, LOW, LOW, LOW, LOW};
+
+unsigned long lastDebounceTimes[LENGTH] = {0, 0, 0, 0, 0, 0};
+unsigned long debounceDelay = 50;
+
+const int numberOfLeds = 6;
+const int ledPin = 9;
+int ledStates[LENGTH] = {LOW, LOW, LOW, LOW, LOW, LOW};
+
+
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(numberOfLeds, ledPin, NEO_GRB + NEO_KHZ800);
+
 void setup() {
   Serial.begin(9600);
   SPI.begin();
   mfrc522.PCD_Init();
-  mfrc522.PCD_SetRegisterBitMask(mfrc522.RFCfgReg, (0x07<<4)); // Enhance the MFRC522 Receiver Gain to maximum value of some 48 dB
+  mfrc522.PCD_SetRegisterBitMask(mfrc522.RFCfgReg, (0x07 << 4)); // Enhance the MFRC522 Receiver Gain to maximum value of some 48 dB
   pinMode(RFIDLED, OUTPUT);
   Ethernet.begin(mac, myip);
   delay(5000); // wait for ethernetcard
   aktivateEthernetSPI(false);
 
   // Machine individual setups go here
+  
+  // Initiate LEDs
+  leds.begin();
+  leds.show();
+
+  // Set pinMode on buttons
+  for (int i = 0; i < LENGTH; i++) {
+    pinMode(buttonPins[i], INPUT);
+  }
+
+
 }
 
 void loop() {
@@ -128,20 +169,20 @@ void getID() {
 
 void submitData(String val) {
 
-  if(val != "") {
+  if (val != "") {
     aktivateEthernetSPI(true);
 
     //String datastring= "GET /start_notepad_exe.php HTTP/1.0";
     //String datastring= "GET /arduino.php?val="+String(val)+" HTTP/1.0";
 
     // IF DEV:
-    String datastring="GET /DEV/pillmachine/machine//setval.php?tag="+String(cardID)+"&maskine="+String(maskinNR)+"&val="+val+" HTTP/1.0";
+    String datastring = "GET /DEV/pillmachine/machine//setval.php?tag=" + String(cardID) + "&maskine=" + String(maskinNR) + "&val=" + val + " HTTP/1.0";
     // IF PRODUCTION:
     // String datastring="GET /machine//setval.php?tag="+String(cardID)+"&maskine="+String(maskinNR)+"&val="+val+" HTTP/1.0";
 
     Serial.println(datastring);
 
-    if(client.connect(pc_server,80)) {
+    if (client.connect(pc_server, 80)) {
       client.println(datastring);
       client.println("Connection: close");
       client.println(); //vigtigt at sende tom linie
@@ -159,23 +200,48 @@ void aktivateEthernetSPI(boolean x) {
   // skift SPI/Slave... turn RFID shield off, ethernet on (LOW=on)
   // http://tronixstuff.com/2011/05/13/tutorial-arduino-and-the-spi-bus/
 
-  digitalWrite(SS_PIN,x);
-  digitalWrite(10,!x);
+  digitalWrite(SS_PIN, x);
+  digitalWrite(10, !x);
 }
 
 void resetData() {
-  userval="";
+  userval = "";
   // Reset your variables here
 }
 
 // this is where the user interface is responsive
 void UI() {
-  /* analog input = A0, A1, A2, A3, A4, A5 (all)
-  * pwm output = D3, D5, D5
-  * digital I/O = D0, D1, D4 + (D3, D5, D5)
-  */
+ 
 
-  userval="28,96,57,70";
+  // Read buttonstates - using the debounce method (see Arduino examples --> 02. Digital --> Debounce)
+  for (int i = 0; i < LENGTH; i++) {
+    int reading = digitalRead(buttonPins[i]);
+    if(reading != lastButtonStates[i]) {
+      lastDebounceTimes[i] = millis();
+    }
+
+    if((millis() - lastDebounceTimes[i]) > debounceDelay) {
+      if(reading != buttonStates[i]) {
+        buttonStates[i] = reading;
+
+        if(buttonStates[i] == HIGH) {
+          ledStates[i] = !ledStates[i];
+        }
+      }
+    }
+
+    // Set LEDs accordingly
+    uint32_t c = ledStates[i] == HIGH ? leds.Color(150, 150, 150) : leds.Color(0, 0, 0);
+    leds.setPixelColor(i, c);
+    leds.show();
+    delay(50);
+
+    lastButtonStates[i] = reading;
+
+  }
+
+
+  userval = "28,96,57,70";
 
   // HUSK AT SÆTTE userval="" HVIS MASKINEN IKKE ER SAT TIL NOGET
 }
