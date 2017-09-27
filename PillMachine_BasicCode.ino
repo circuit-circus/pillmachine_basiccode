38a68cb8895f787975c34558d00862c452a8fdaf
Jesper Fogh <foghjesperjhf@gmail.com>
2017-09-27 18:00:30 +0200

    Updated machine number and IP

--- PillMachine_BasicCode.ino
+++ PillMachine_BasicCode.ino
@@ -29,7 +29,7 @@ moja & circuitcircus
 #include <Servo.h>
 #include <FastLED.h>
 
-#define maskinNR 1 //FOR AT VI VED HVILKEN STATION DER SUBMITTER
+#define maskinNR 9 //FOR AT VI VED HVILKEN STATION DER SUBMITTER
 
 #define SS_PIN 8 // SDA for RFID
 #define RST_PIN 9 // RST
@@ -37,7 +37,7 @@ moja & circuitcircus
 #define RFIDLED 2 // LEDLIGHT BEHIND RFID TAG ---- brug pin 0 eller 1 i endelig version
 
 static uint8_t mac[] = {  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEF };
-static uint8_t myip[] = {  10, 0, 0, 100 };
+static uint8_t myip[] = {  10, 0, 0, 109 };
 IPAddress pc_server(10,0,0,31);  // serverens adress
 
 boolean cardPresent = false; // DEBUG: Set this and isDebugging to true to test UI
